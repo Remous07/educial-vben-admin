771734198aee8f505a2205b139e914b886ad0ad6
vmox <email@vmox.net>
2026-09-09 18:09:07 +0800

    style: 右上角头像在线状态点回落到默认尺寸 (size-4 → size-3)
    去掉 avatarDotClass 里的 size-4，让绿点使用 avatar 组件默认的 size-3
    （12px），避免 16px 绿点在 32px 头像上显得过大。

--- packages/effects/layouts/src/widgets/user-dropdown/user-dropdown.vue
+++ packages/effects/layouts/src/widgets/user-dropdown/user-dropdown.vue
@@ -102,7 +102,7 @@ defineOptions({
 const props = withDefaults(defineProps<Props>(), {
   avatar: '',
   avatarDot: true,
-  avatarDotClass: 'bottom-0 right-1 border-2 size-4 bg-green-500',
+  avatarDotClass: 'bottom-0 right-1 border-2 bg-green-500',
   description: '',
   menus: () => [],
   tagText: '',
